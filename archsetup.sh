f118f9c22d7e736e282356148409242d713a7d2e
Spencer <spencerkrebs1992@gmail.com>
2022-09-15 09:05:02 -0700

    initial commit

---
+++ archsetup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#install blackarch repository
+
+# Run https://blackarch.org/strap.sh as root and follow the instructions.
+curl -O https://blackarch.org/strap.sh
+
+# Set execute bit
+chmod +x strap.sh
+
+# Run strap.sh
+sudo ./strap.sh
+
+# Enable multilib following https://wiki.archlinux.org/index.php/Official_repositories#Enabling_multilib and run:
+sudo pacman --noconfirm -Syu
+
+#install regular tools
+sudo pacman -S --no-confirm git flameshot terminator
+
+#install pentest tools
+sudo pacman -S --noconfirm john enum4linux metasploit aircrack-ng arptools arpscan sqlmap burpsuite cupp cowpatty gobuster dirbuster fierce nikto hydra sherlock set transmission-gtk nmap empire dnsutils
+
+# install z shell
+sudo pacman -Syu
+sudo pacman -S --noconfirm #!/usr/bin/env zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
