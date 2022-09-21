b2f6f2198af352612c24b0179775e70fe9a83397
Spencer <spencerkrebs1992@gmail.com>
2022-09-21 15:31:33 -0700

    updated script

--- archsetup.sh
+++ archsetup.sh
@@ -20,6 +20,14 @@ sudo pacman -S --no-confirm git flameshot terminator
 #install pentest tools
 sudo pacman -S --noconfirm john enum4linux metasploit aircrack-ng arptools arpscan sqlmap burpsuite cupp cowpatty gobuster dirbuster fierce nikto hydra sherlock set transmission-gtk nmap empire dnsutils
 
+# Create Folders
+mkdir ~/Documents/Scripts
+mkdir ~/Documents/Wordlists
+
+#install some tools from github
+sudo git clone https://github.com/pentestmonkey/php-reverse-shell.git ~/Documents/Scripts
+sudo git clone https://github.com/danielmiessler/SecLists.git ~/Documents/Wordlists
+
 # install z shell
 sudo pacman -Syu
 sudo pacman -S --noconfirm #!/usr/bin/env zsh
